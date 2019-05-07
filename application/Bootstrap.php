bd2d6ed22c1496fa70bcb21858961e60613943c7
ddomen <daniele.domenichelli.5@gmail.com>
2019-05-07 17:33:35 +0200

    Aggiunto Layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,32 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+    protected $_view;
+    protected $_layout;
 
+    // Inizializzazione FrontController
+    protected function _initRequest() {
+        $this->bootstrap('FrontController');
+        $front = $this->getResource('FrontController');
+        $request = new Zend_Controller_Request_Http();
+        $front->setRequest($request);
+    }
 
+    // Inizializzazione View
+    protected function _initViewSettings() {
+        $this->bootstrap('view');
+        $this->_view = $this->getResource('view');
+        $this->_view->headMeta()->setCharset('UTF-8');
+        $this->_view->headMeta()->appendHttpEquiv('Content-Language', 'it-IT');
+	    $this->_view->headLink()->appendStylesheet($this->_view->baseUrl('css/style.css'));
+        $this->_view->headScript()->appendFile($this->_view->baseUrl('js/jquery.js'));
+        $this->_view->headTitle('Noleggio Macchine');
+    }
+
+    // Inizializzazione Layout
+    protected function _initLayoutSettings(){
+        $this->bootstrap('layout');
+        $this->_layout = $this->getResource('layout');
+    }
 }
 
