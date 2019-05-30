74dacc292b59571c85cd75129b66dc6d1f33df70
ddomen <daniele.domenichelli.5@gmail.com>
2019-05-30 17:46:47 +0200

    aggiunto invio messaggi

--- public/js/messanger.js
+++ public/js/messanger.js
@@ -1,10 +1,20 @@
 $(document).ready(()=>{
     $icon = $('.app-messanger-icon');
     $chat = $('.app-messanger-chat');
-
-    $chat.hide();
+    $chat_container = $chat.find('.app-messanger-chat-container');
+    $chat_text = $chat.find('textarea');
+    $chat_send = $chat.find('button');
 
     $icon.click(()=>{
         $chat.toggle(200);
+        $chat_container.scrollTop(10000);
+    })
+    $chat_send.click(()=>{
+        $msg = $('<p>');
+        $msg.addClass('app-messanger-message').addClass('app-messanger-message-outgoing');
+        $msg.text($chat_text.val());
+
+        $chat_container.append($msg);
+        $chat_container.scrollTop(10000);
     })
 })
