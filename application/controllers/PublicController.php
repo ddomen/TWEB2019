c4754c0209c3775d91906fca1a24c7820c051083
LucaChiavarini <lucachiavarini@gmail.com>
2019-05-30 14:40:49 +0200

    contattaci e aboutus

--- application/controllers/PublicController.php
+++ application/controllers/PublicController.php
@@ -4,11 +4,19 @@ class PublicController extends Zend_Controller_Action
 {
 
     public function init() {
-        /* Initialize action controller here */
+        /* Initialize action controller here */ 
     }
 
     public function indexAction() {
         // action body
+    }
+    
+    public function aboutusAction(){
+           
+    }
+          
+    public function contattaciAction(){
+        
     }
 
 
