d39e37b946392b6fcb5ec47d8fcf22d626d163b0
LucaChiavarini <lucachiavarini@gmail.com>
2019-06-12 10:48:39 +0200

    aggiustato filtro catalogo

--- application/forms/Public/Catalog/Filter.php
+++ application/forms/Public/Catalog/Filter.php
@@ -14,7 +14,7 @@ class Application_Form_Public_Catalog_Filter extends Zend_Form
             $this->addElement('text', 'modello', array(
             'label' => 'Modello',
             'filters' => array('StringTrim'),
-            'required' => true,
+            'required' => false,
             'description' => 'Modello della macchina',
             'validators' => array(array('StringLength',true, array(1,25))),
 		));          
@@ -22,21 +22,21 @@ class Application_Form_Public_Catalog_Filter extends Zend_Form
             $this->addElement('text', 'marca', array(
             'label' => 'Marca',
             'filters' => array('StringTrim'),
-            'required' => true,
+            'required' => false,
             'description' => 'Marca della macchina',
             'validators' => array(array('StringLength',true, array(1,25))),
 		)); 
             
             $this->addElement('text', 'prezzoMin', array(
             'label' => 'Prezzo Minimo',
-            'required' => true,
+            'required' => false,
             'filters' => array('LocalizedToNormalized'),
             'validators' => array(array('Float', true, array('locale' => 'en_US'))),
 		));
             
             $this->addElement('text', 'prezzoMax', array(
             'label' => 'Prezzo Massimo',
-            'required' => true,
+            'required' => false,
             'filters' => array('LocalizedToNormalized'),
             'validators' => array(array('Float', true, array('locale' => 'en_US'))),
 		));
@@ -44,11 +44,16 @@ class Application_Form_Public_Catalog_Filter extends Zend_Form
             $this->addElement('text', 'posti', array(
             'label' => 'Posti',
             'filters' => array('StringTrim'),
-            'required' => true,
+            'required' => false,
             'validators' => array(array('StringLength',true, array(1,25))),
 		));
           
-            
+            $this->addElement('text', 'allestimento', array(
+            'label' => 'Allestimento',
+            'filters' => array('StringTrim'),
+            'required' => false,
+            'validators' => array(array('StringLength',true, array(1,100))),
+		));
             
             
         
--- application/models/resources/Macchine.php
+++ application/models/resources/Macchine.php
@@ -4,7 +4,7 @@ class Application_Resource_Macchine extends Zend_Db_Table_Abstract {
     protected $_name    = 'macchine';
     protected $_primary  = 'ID';
     protected $_rowClass = 'Application_Resource_Macchine_Item';
-
+    
     public function init() { }
     
     
@@ -43,25 +43,39 @@ class Application_Resource_Macchine extends Zend_Db_Table_Abstract {
     
     
     public function getCatalogFiltered($values){
-             
-        $prezzoMin=$values[prezzoMin];
-        $prezzoMax=$values[prezzoMax];
-        $select = $this->select()->where('modello IN (?)',$values)
-                                 ->where('marca IN (?)', $values)
-                                 ->where('prezzo > ?', $prezzoMin)
-                                 ->where('prezzo < ?', $prezzoMax)
-                                 ->where('posti IN (?)', $values);
+        $select = $this->select();
         
+        if($values[modello]!=null){
+            $select->where('modello LIKE ?','%'. $values[modello] .'%');
+        }
         
+        if($values[marca]!=null){
+            $select->where('marca LIKE ?','%'. $values[marca] .'%');
+        }
         
+        if($values[prezzoMin]!=null){
+            $prezzoMin=$values[prezzoMin];
+            $select->where('prezzo > ?', $prezzoMin);
+        }
+        
+        if($values[prezzoMax]!=null){
+            $prezzoMax=$values[prezzoMax];
+            $select->where('prezzo < ?', $prezzoMax);
+        }
+        
+        if($values[posti]!=null){
+            $select->where('posti =?', $values[posti]);
+        }
+        
+        if($values[allestimento]!=null){
+            $select->where('allestimento LIKE ?','%'. $values[allestimento] .'%');
+        }
         
         return $this->fetchAll($select);
+        
+        
     } 
-    
-    
-    
 }
 
 
-    
 
